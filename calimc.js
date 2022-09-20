306417c97d5313f2bb8719807abf94be02cf49b0
Vinicius Braz <105358937+vinicius-braz1@users.noreply.github.com>
2022-09-20 10:57:00 -0300

    Add files via upload

---
+++ calimc.js
@@ -0,0 +1,11 @@
+var peso=prompt("digite o peso");{
+var altura=prompt("digite a altura");
+var imc=peso/(altura^2);
+if(imc<18){
+    alert("abaixo do peso")
+}else if(imc>=18>=30){
+    alert("peso ideal")
+}else{
+    alert("acima do peso")
+}
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+var nomedavariavel="valor da variavel";
+var outravariavel='valor da outra variavel';
+var variavel=15;
+var variavelvouganharnamega=false;
+var variavelindefinida;
+console.log(nomedavariavel);
+var teste="Aprender";
+console.log(teste);
+teste="novovalor";
+console.log(teste);
